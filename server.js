cf557cb30e514780cdd5f7af975c837350788b58
dok <dumansky@gmail.com>
2018-05-29 22:42:25 +0300

    allow origin headers

--- server.js
+++ server.js
@@ -18,25 +18,13 @@ var storage = multer.diskStorage({
     }
 });
 
-var upload = multer({ storage: storage });
+var upload = multer({ storage: storage }).single('file');
 
 
 app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
@@ -50,7 +38,8 @@ app.get('/', function (req, res) {
 });
 
 function uploadMiddleware(req, res) {
-    upload.single('file')(req, res, function(err) {
+    upload(req, res, function(err) {
+    // upload.any()(req, res, function(err) {
         if(err){
             console.log(err);
             res.json(err);
